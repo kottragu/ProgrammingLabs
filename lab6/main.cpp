756ec064faab2227a1e9fd13a8f344aea7c70a03
kottragu <62839457+kottragu@users.noreply.github.com>
2020-05-13 14:50:38 +0300

    Update main.cpp

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
 #include <vector>
 #include "second.h"
-#include "second.cpp"
 using namespace std;
+
+template <typename Z>
+class Dot{
+private:
+    Z x;
+    Z y;
+public:
+    template <typename TR>
+    Dot(TR x1, TR y1){
+        x = x1;
+        y = y1;
+    }
+    template <typename T>
+    bool operator <(T num){
+        return x < num;
+    }
+};
+template <typename T>
+bool fir(T x){
+    return x < 0;
+}
 
 int main() {
 
     vector<int> v{1, 0, 3};
-    vector<char> str {'a', 'b', 'c'};
-    vector<Dot> v1{{6,1},
-                   {6,2},
-                   {6,1}
-    };
+    vector<Dot<int>> v1{{6,1}, {6,2}, {6,1} };
 
     cout << "all_of: ";
-    if(all_of(v.begin(), v.end(), func_1)){
+    if(all_of(v.begin(), v.end(), fir<int>)){
         cout << "true ";
     } else {
         cout << "false ";
     }
-    if(all_of(v1.begin(), v1.end(), func_2)){
+    if (all_of(v1.begin(), v1.end(), fir<Dot<int>>)) {
         cout << "true" << endl;
     } else {
         cout << "false" << endl;
@@ -27,29 +43,29 @@ int main() {
 
 
     cout << "is_sorted: ";
-    if(is_sorted(v.begin(), v.end(), func_sort_1)) {
+    if(is_sorted(v.begin(), v.end(), fir<int>)) {
         cout << "true ";
     } else {
         cout << "false ";
     }
-    if(is_sorted(v1.begin(), v1.end(), func_sort_2)) {
+    if(is_sorted(v1.begin(), v1.end(), fir<Dot<int>>)) {
         cout << "true" << endl;
     } else {
         cout << "false" << endl;
     }
+
 
     cout << "is_palindrome: ";
-    if(is_palindrome(str.begin(),str.end(),is_palindrome_1)){
+    if(is_palindrome(v.begin(), v.end(), fir<int>)){
         cout << "true ";
     } else {
         cout << "false ";
     }
-    if(is_palindrome(v1.begin(),v1.end(),is_palindrome_2)){
+    if(is_palindrome(v1.begin(),v1.end(), fir<Dot<int>>)){
         cout << "true" << endl;
     } else {
         cout << "false" << endl;
     }
-
     return 0;
 }
 
